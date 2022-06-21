bdbc3f66d42fa402dceadf06b5b5d1b62054615a
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-21 11:58:59 +0200

    all items in, search works

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import axios from 'axios';
 import { Noun } from './components/Noun';
 import { Book } from './components/Book';
 import { TechPerson } from './components/TechPerson';
+import { Setting } from './components/Setting';
+import { Employee } from './components/Employee';
+import { Translation } from './components/Translation';
+import { Job } from './components/Job';
+import { LandscapePhoto } from './components/LandscapePhoto';
 
-const url = 'http://localhost:3007/all';
+const baseUrl = 'http://localhost:3007';
+const url = `${baseUrl}/all`;
 const separator = '|';
 
 function App() {
@@ -46,19 +52,74 @@ function App() {
                 });
             });
 
+            Object.entries(_siteData.settings).forEach((entry) => {
+                const key = entry[0];
+                const value = entry[1];
+                _searchItems.push({
+                    kind: 'setting',
+                    bulkSearch: key + separator + value,
+                    item: {
+                        key,
+                        value
+                    }
+                });
+            });
+
+            _siteData.employees.forEach((item) => {
+                _searchItems.push({
+                    kind: 'employee',
+                    bulkSearch: item.FIRST_NAME + separator + item.LAST_NAME,
+                    item
+                });
+            });
+
+            _siteData.translations.forEach((item) => {
+                _searchItems.push({
+                    kind: 'translation',
+                    bulkSearch:
+                        item.fromLanguage +
+                        separator +
+                        item.toLanguage +
+                        separator +
+                        item.fromPhrase +
+                        separator +
+                        item.toPhrase,
+                    item
+                });
+            });
+
+            _siteData.jobs.forEach((item) => {
+                _searchItems.push({
+                    kind: 'job',
+                    bulkSearch: item.html,
+                    item
+                });
+            });
+
+            _siteData.landscapePhotos.forEach((item) => {
+                _searchItems.push({
+                    kind: 'landscapePhoto',
+                    bulkSearch: item,
+                    item
+                });
+            });
+
             setSearchItems(_searchItems);
             setFilteredSearchItems([]);
         })();
     }, []);
 
     const handleSearch = (e) => {
-        const searchText = e.target.value;
-        if (searchText === '') {
-            setFilteredSearchItems([]);
-        } else {
-            const _filteredSearchItems = searchItems.filter((m) =>
+        const searchText = e.target.value.trim();
+        let _filteredSearchItems = [];
+        if (searchText.length >= 3) {
+            _filteredSearchItems = searchItems.filter((m) =>
                 m.bulkSearch.toLowerCase().includes(searchText.toLowerCase())
             );
+            setFilteredSearchItems(_filteredSearchItems);
+        }
+        if (searchText === '') {
+            _filteredSearchItems = [];
             setFilteredSearchItems(_filteredSearchItems);
         }
     };
@@ -87,6 +148,25 @@ function App() {
                                 )}
                                 {item.kind === 'techPerson' && (
                                     <TechPerson item={item.item} />
+                                )}
+
+                                {item.kind === 'setting' && (
+                                    <Setting item={item.item} />
+                                )}
+                                {item.kind === 'employee' && (
+                                    <Employee item={item.item} />
+                                )}
+                                {item.kind === 'translation' && (
+                                    <Translation item={item.item} />
+                                )}
+                                {item.kind === 'job' && (
+                                    <Job item={item.item} />
+                                )}
+                                {item.kind === 'landscapePhoto' && (
+                                    <LandscapePhoto
+                                        item={item.item}
+                                        baseUrl={baseUrl}
+                                    />
                                 )}
                             </>
                         );
