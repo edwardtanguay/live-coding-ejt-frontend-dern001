c432959cfb780fb029e22a86d00f7ec32f1fcbbc
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-21 11:04:02 +0200

    getting /all and showing nouns

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const url = 'http://localhost:3007/all';
 
 function App() {
+    const [siteData, setSiteData] = useState({});
+
+    useEffect(() => {
+        (async () => {
+            const _siteData = (await axios.get(url)).data;
+            setSiteData(_siteData);
+        })();
+    }, []);
+
     return (
         <div className="App">
             <div>Testing</div>
+            {Object.keys(siteData).length === 0 ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    <div>There are {siteData.nouns.length} nouns.</div>
+                    {siteData.nouns.map((noun, i) => {
+                        return <li key={i}>{noun.singular}</li>;
+                    })}
+                </>
+            )}
         </div>
     );
 }
